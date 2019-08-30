4d5a6dedd7d60249521fea54820b3e1684ec068a
David Zhang <zdavid0123@gmail.com>
2019-08-30 11:51:31 -0400

    keep allele order

--- splitrefstu.sh
+++ splitrefstu.sh
@@ -6,9 +6,9 @@ nstu=$2
 
 cat $inpref.fam | sort -R > tmp
 cat tmp | head -n $nstu > tmp_stu
-plink --bfile $inpref --keep tmp_stu --make-bed --out ${inpref}_stu
-plink --bfile $inpref --remove tmp_stu --make-bed --out ${inpref}_ref
-plink --bfile ${inpref}_stu --recode vcf-iid --out ${inpref}_stu
-plink --bfile ${inpref}_ref --recode vcf-iid --out ${inpref}_ref
+plink --bfile $inpref --keep-allele-order --keep tmp_stu --make-bed --out ${inpref}_stu
+plink --bfile $inpref --keep-allele-order --remove tmp_stu --make-bed --out ${inpref}_ref
+plink --bfile ${inpref}_stu --keep-allele-order --recode vcf-iid --out ${inpref}_stu
+plink --bfile ${inpref}_ref --keep-allele-order --recode vcf-iid --out ${inpref}_ref
 
 rm tmp tmp_stu
