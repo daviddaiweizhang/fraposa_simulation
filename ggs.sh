5e1b71afdc3a8078aa5c0b0b1181470111a3fb6e
David Zhang <zdavid0123@gmail.com>
2019-08-29 23:18:53 -0400

    add ggs.sh

---
+++ ggs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+n=$1
+outfile=$2
+
+K=2 # sidelength of grid
+M=100 # migration rate
+L=100 # number of loci in a genealogy
+G=1000 # number of independent genealogies
+let "C = $n * 2" # number of haplid samples per center
+ggs=~/ggs/ggs
+
+$ggs -K $K -c $C -M $M -G $G -L $L -o $outfile
