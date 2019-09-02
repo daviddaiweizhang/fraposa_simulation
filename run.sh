40add8ac8f0c20946c5eda43f90c1e9fe7595f5f
David Zhang <zdavid0123@gmail.com>
2019-09-02 00:37:58 -0400

    pass nstu

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+nstu=200
+
 mkdir -p log
 for n in `seq 1000 500 3000`; do
     let "npercenter = ($n + 200) / 4"
-    bash run.sh $npercenter &> log/c$npercenter.log & 
+    bash simulate.sh $npercenter $nstu &> log/c$npercenter.log &
 done
