be62d806b1c8de0e8f5d08a17688c73950016181
David Zhang <zdavid0123@gmail.com>
2019-09-02 14:16:34 -0400

    get population for trace pcs

--- trace.sh
+++ trace.sh
@@ -13,5 +13,6 @@ trace=~/laser/trace
 $vcf2geno --inVcf $refpref.vcf --out $refpref
 $vcf2geno --inVcf $stupref.vcf --out $stupref
 $trace -s $stupref.geno -g $refpref.geno -o $outpref -k $k -K $K
-cat $outpref.ProPC.coord | tail -n +2 | rev | cut -f-$k | rev > ${outpref}_adp.pcs
+cat $outpref.ProPC.coord | tail -n +2 | cut -f2,7- > ${outpref}_tmp
+cat ${outpref}_tmp | cut -d'_' -f-2 | paste - ${outpref}_tmp > ${outpref}_adp.pcs
 rm -f trace.conf
