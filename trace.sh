9cfcc8ddd70e35d34e1d185a0d09d4b8dde11e4f
David Zhang <zdavid0123@gmail.com>
2019-09-01 23:45:56 -0400

    convert .coord to .pcs

--- trace.sh
+++ trace.sh
@@ -10,7 +10,8 @@ outpref=$5
 vcf2geno=~/laser/vcf2geno/vcf2geno
 trace=~/laser/trace
 
-$vcf2geno --inVcf $refpref.vcf --out $refpref
-$vcf2geno --inVcf $stupref.vcf --out $stupref
-$trace -s $stupref.geno -g $refpref.geno -o $outpref -k $k -K $K
+# $vcf2geno --inVcf $refpref.vcf --out $refpref
+# $vcf2geno --inVcf $stupref.vcf --out $stupref
+# $trace -s $stupref.geno -g $refpref.geno -o $outpref -k $k -K $K
+cat $outpref.ProPC.coord | tail -n +2 | rev | cut -f-$k | rev > ${outpref}_adp.pcs
 rm -f trace.conf
