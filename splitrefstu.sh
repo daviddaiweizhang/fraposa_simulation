ed7cc69f618b68e18ac9341682560afa1fceb172
David Zhang <zdavid0123@gmail.com>
2019-09-02 00:38:37 -0400

    save splitrefstu.sh tmp files in output dir

--- splitrefstu.sh
+++ splitrefstu.sh
@@ -11,11 +11,11 @@ get_seeded_random()
     openssl enc -aes-256-ctr -pass pass:"$seed" -nosalt </dev/zero 2>/dev/null
 }
 
-cat $inpref.fam | shuf --random-source=<(get_seeded_random $seed) > tmp
-cat tmp | head -n $nstu > tmp_stu
-plink --bfile $inpref --keep-allele-order --keep tmp_stu --make-bed --out ${inpref}_stu
-plink --bfile $inpref --keep-allele-order --remove tmp_stu --make-bed --out ${inpref}_ref
+cat $inpref.fam | shuf --random-source=<(get_seeded_random $seed) > ${inpref}_tmp
+cat ${inpref}_tmp | head -n $nstu > ${inpref}_tmp_stu
+plink --bfile $inpref --keep-allele-order --keep ${inpref}_tmp_stu --make-bed --out ${inpref}_stu
+plink --bfile $inpref --keep-allele-order --remove ${inpref}_tmp_stu --make-bed --out ${inpref}_ref
 plink --bfile ${inpref}_stu --keep-allele-order --recode vcf-iid --out ${inpref}_stu
 plink --bfile ${inpref}_ref --keep-allele-order --recode vcf-iid --out ${inpref}_ref
 
-rm tmp tmp_stu
+rm ${inpref}_tmp ${inpref}_tmp_stu
