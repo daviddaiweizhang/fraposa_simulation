9a1cec0d6d233758ca0fc6950e8c8723d129035e
David Zhang <zdavid0123@gmail.com>
2019-09-02 16:47:18 -0400

    produce _ref.pcs

--- trace.sh
+++ trace.sh
@@ -15,4 +15,6 @@ $vcf2geno --inVcf $stupref.vcf --out $stupref
 $trace -s $stupref.geno -g $refpref.geno -o $outpref -k $k -K $K
 cat $outpref.ProPC.coord | tail -n +2 | cut -f2,7- > ${outpref}_tmp
 cat ${outpref}_tmp | cut -d'_' -f-2 | paste - ${outpref}_tmp > ${outpref}_adp.pcs
+cat $outpref.RefPC.coord | tail -n +2 | cut -f2- > ${outpref}_tmp
+cat ${outpref}_tmp | cut -d'_' -f-2 | paste - ${outpref}_tmp > ${outpref}_ref.pcs
 rm -f trace.conf
