4dcb7fb0fe1487f29f80ad4f10ecb9fc2e98d59c
David Zhang <zdavid0123@gmail.com>
2019-09-02 00:38:20 -0400

    save tmp files in output dir

--- ggs2plink.sh
+++ ggs2plink.sh
@@ -5,13 +5,13 @@ infile=$1
 outpref=$2
 
 echo 'producing .tped file...'
-cat $infile | tail -n +2 | cut -f2- | sed 's/0/A/g' | sed 's/1/C/g' > tmp
-cat $infile | tail -n +2 | cut -f1 | sed 's/,/_/' | sed 's/^/1\t/' | sed 's/$/\t0/' | nawk '{print $0 "\t" FNR}' > tmp_leader
-paste tmp_leader tmp | sed 's/\t/ /g' > $outpref.tped
+cat $infile | tail -n +2 | cut -f2- | sed 's/0/A/g' | sed 's/1/C/g' > ${outpref}_tmp
+cat $infile | tail -n +2 | cut -f1 | sed 's/,/_/' | sed 's/^/1\t/' | sed 's/$/\t0/' | nawk '{print $0 "\t" FNR}' > ${outpref}_tmp_leader
+paste ${outpref}_tmp_leader ${outpref}_tmp | sed 's/\t/ /g' > $outpref.tped
 
 echo 'producing .tfam file...'
 cat $infile | head -n 1 | cut -f2- | sed 's/\//_/g' | sed 's/\t/\n/g' | awk 'NR % 2 == 1' | awk '{ print $1 " " $1 }' | sed 's/$/ 0 0 1 1/' | head -n -1 > $outpref.tfam
 
 plink --tfile $outpref --make-bed --out $outpref
 
-rm tmp tmp_leader 
+rm ${outpref}_tmp ${outpref}_tmp_leader 
