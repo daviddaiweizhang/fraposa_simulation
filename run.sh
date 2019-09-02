009973c3ead1e87450644057b00f8660000533a4
David Zhang <zdavid0123@gmail.com>
2019-09-02 14:18:31 -0400

    rename variables in run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-nstu=200
+nstu=50
 
 mkdir -p log
-for n in `seq 1000 500 3000`; do
-    let "npercenter = ($n + 200) / 4"
-    bash simulate.sh $npercenter $nstu &> log/c$npercenter.log &
+for m in `seq 1000 500 3000`; do
+    let "n = $m / 4 + 50"
+    bash simulate.sh $n $nstu &> log/c$n.log &
 done
