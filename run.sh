6871369bf2f891d9ba2e24aa0733fbf5a5588e9f
David Zhang <zdavid0123@gmail.com>
2019-09-02 22:54:58 -0400

    plot msd

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
-nstu=50
+nstu=100
+msd=data/msd
+rm -f $msd
 
 mkdir -p log
 for m in `seq 1000 500 3000`; do
     let "n = $m / 4 + 50"
-    bash simulate.sh $n $nstu &> log/c$n.log &
+    pref=data/c${n}/a
+    bash simulate.sh $n $nstu $pref &> log/c$n.log &
+    cat ${pref}_msd >> $msd
 done
+
+Rscript plot_trend.R $msd
