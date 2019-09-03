fe00ef52429a4bbf3ac982ea2cca584778dec614
David Zhang <zdavid0123@gmail.com>
2019-09-02 22:54:00 -0400

    add plot_trend.R

---
+++ plot_trend.R
@@ -0,0 +1,23 @@
+rm(list=ls())
+args = commandArgs(trailingOnly=TRUE)
+msd.pref = ifelse(length(args) >= 1, args[1], "data/msd")
+x = read.table(msd.pref)
+methods = c("sp", "ap", "oadp", "adp")
+colnames(x) = c("nref", methods)
+
+lwd = 3
+cex = 2
+png(paste0(msd.pref, ".png"), 1000, 1000)
+par(cex=2)
+for(i in 1:length(methods)){
+    method = methods[i]
+    if(i == 1){
+        xlab = "Reference size"
+        ylab = "Squared root of MSD between centers"
+        plot(x$nref, x[[method]], type="b", col=i, ylim=c(0, max(x[[method]])), xlab=xlab, ylab=ylab, lwd=lwd, cex=cex)
+    } else{
+        points(x$nref, x[[method]], type="b", col=i, lwd=lwd, cex=cex)
+    }
+}
+legend("topright", legend=methods, pch=1, lty=1, col=1:4)
+dev.off()
