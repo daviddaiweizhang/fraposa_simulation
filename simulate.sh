8c165cbb7d957b08847f29e94a597de71e9c4db9
David Zhang <zdavid0123@gmail.com>
2019-09-02 22:54:45 -0400

    pass pref as argument

--- simulate.sh
+++ simulate.sh
@@ -3,9 +3,9 @@ set -e
 
 n=$1
 nstu=$2
+pref=$3
 K=8
 k=2
-pref=data/c${n}/a
 mkdir -p `dirname $pref`
 
 bash ggs.sh $n $pref.ggs
@@ -15,4 +15,4 @@ bash fraposa.sh ${pref}_ref ${pref}_stu oadp ${pref}_oadp
 bash fraposa.sh ${pref}_ref ${pref}_stu ap ${pref}_ap
 bash fraposa.sh ${pref}_ref ${pref}_stu sp ${pref}_sp
 bash trace.sh ${pref}_ref ${pref}_stu $K $k $pref
-Rscript plot.R $pref
+Rscript plot_pcs.R $pref
