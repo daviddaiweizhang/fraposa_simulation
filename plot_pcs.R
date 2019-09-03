c0d7dd6f343da80e42183f6f52aca121d7b886b5
David Zhang <zdavid0123@gmail.com>
2019-09-03 19:18:16 -0400

    adjust for ref pcs sign difference, compute null dist for msd

--- plot_pcs.R
+++ plot_pcs.R
@@ -5,41 +5,76 @@ radius = 0.9
 methods = c("sp", "ap", "oadp", "adp")
 lim = c(-35, 35)
 
+# read reference samples
 args = commandArgs(trailingOnly=TRUE)
-inpref = ifelse(length(args) >= 1, args[1], "data/c800/a")
+inpref = ifelse(length(args) >= 1, args[1], "data/c300/a")
 x.ref = read.table(paste0(inpref, "_ref.pcs"))
 colnames(x.ref) = c("popu", "id", "PC1", "PC2")
-x.ref = x.ref[order(x.ref$popu),]
+# x.ref = x.ref[order(x.ref$popu),]
 
-# rotate the samples
+# get reference centers
 c.ref = aggregate(x.ref[,c("PC1", "PC2")], by = list(x.ref$popu), FUN = mean)
 colnames(c.ref) = c("popu", "C1", "C2")
-aa = c.ref[1:2, 2:3]
-aa = aa / sqrt(rowSums(aa^2))
-bb = 1/sqrt(2) * matrix(c(1,-1,1,1), 2, 2)
-rot = as.matrix(solve(aa) %*% bb)
+
+# adjust for ref pcs sign difference
+x.ref2 = read.table(paste0(inpref, "_ref_Vs.dat"))
+reflect = sign(x.ref2[1,] / x.ref[1,c("PC1", "PC2")])
+# colnames(x.ref2) = c("PC1", "PC2")
+# c.ref2 = aggregate(x.ref2[,c("PC1", "PC2")], by = list(x.ref$popu), FUN = mean)
+# colnames(c.ref2) = c("popu", "C1", "C2")
+# reflect = sign(c.ref2[,c("C1", "C2")] / c.ref[,c("C1", "C2")])
+reflect = c(as.matrix(reflect))
+
+# rotate the samples
+basis = c.ref[1:2, 2:3]
+basis = basis / sqrt(rowSums(basis^2))
+fortyfive = 1/sqrt(2) * matrix(c(1,-1,1,1), 2, 2)
+rot = as.matrix(solve(basis) %*% fortyfive)
 x.ref[,c("PC1", "PC2")] = as.matrix(x.ref[,c("PC1", "PC2")]) %*% rot
 c.ref[,c("C1", "C2")] = as.matrix(c.ref[,c("C1", "C2")]) %*% rot
 
+# add population center to each sample
 x.ref = merge(x.ref, c.ref)
 x.ref$r = sqrt(rowSums((x.ref[,c("PC1", "PC2")] - x.ref[,c("C1", "C2")])^2))
 c.ref = merge(c.ref, aggregate(data.frame(r=x.ref$r), by=list(popu=x.ref$popu), FUN=function(x) quantile(x, radius)))
+
+# read study samples
+x.stu.all = list()
+for(method in methods){
+    x.stu = read.table(paste0(inpref, "_", method, ".pcs"))
+    colnames(x.stu) = c("popu", "id", "PC1", "PC2")
+    x.stu.all[[method]] = x.stu
+}
+nstu = sum(x.stu.all[[1]]$popu == c.ref$popu[1])
+
+# find the null distribution of MSD
+fun = function(popu){
+    y = x.ref[x.ref$popu == popu,]
+    y = y[sample(nrow(y), nstu),]
+    d = colMeans(y[,c("PC1", "PC2")]) - c.ref[c.ref$popu == popu, c("C1", "C2")]
+    sum(d^2)
+}
+nrep = 100
+msd.nulldist = sapply(1:nrep, function(x) mean(sapply(c.ref$popu, fun)))
+msd.nullmean = mean(msd.nulldist)
 
 msd.all = c(nrow(x.ref))
 png(paste0(inpref, ".png"), 2000, 2000)
 par(mfrow=c(2,2), cex=2)
 for(method in methods){
-    x.stu = read.table(paste0(inpref, "_", method, ".pcs"))
-    colnames(x.stu) = c("popu", "id", "PC1", "PC2")
-    x.stu = x.stu[order(x.stu$popu),]
+    x.stu = x.stu.all[[method]]
+    # x.stu = x.stu[order(x.stu$popu),]
+
+    # adjust for sign flipping
+    if(method %in% c("sp", "ap", "oadp")){
+        # reflect = sign(c.stu[1, c("C1", "C2")] / c.ref[1, c("C1", "C2")])
+        # reflect = c(as.matrix(reflect))
+        x.stu[,c("PC1", "PC2")] = x.stu[,c("PC1", "PC2")] * rep(reflect, each=nrow(x.stu))
+    }
+
     x.stu[,c("PC1", "PC2")] = as.matrix(x.stu[,c("PC1", "PC2")]) %*% rot
     c.stu = aggregate(x.stu[,c("PC1", "PC2")], by = list(x.stu$popu), FUN = mean)
     colnames(c.stu) = c("popu", "C1", "C2")
-    reflect = sign(c.stu[1, c("C1", "C2")] / c.ref[1, c("C1", "C2")])
-    reflect = c(as.matrix(reflect))
-    x.stu[,c("PC1", "PC2")] = x.stu[,c("PC1", "PC2")] * rep(reflect, each=nrow(x.stu))
-    c.stu[,c("C1", "C2")] = c.stu[,c("C1", "C2")] * rep(reflect, each=nrow(c.stu))
-
     stu.pch = 14
     main = paste(method, nrow(x.ref))
     col.ref = as.integer(x.ref$popu) + 1
@@ -58,9 +93,11 @@ for(method in methods){
         des.stu = paste("Popu.", 1:4, "stu.")
         legend("topright", legend=des.stu, pch=(1:4)+stu.pch, col=1)
     }
-    msd = sqrt(mean(rowSums((c.stu[,c("C1", "C2")] - c.ref[,c("C1", "C2")])^2)))
+    msd = mean(rowSums((c.stu[,c("C1", "C2")] - c.ref[,c("C1", "C2")])^2))
     msd = round(msd, 3)
-    legend("bottomleft", legend=paste("MSD:", msd))
+    relmsd = msd / msd.nullmean
+    relmsd = round(relmsd, 3)
+    legend("bottomleft", legend=paste(c("abs.", "rel."), "MSD:", c(msd, relmsd)))
     msd.all = c(msd.all, msd)
 }
 dev.off()
