9e5634c48be59bf094ab52fda3ae6bfc3730a8de
David Zhang <zdavid0123@gmail.com>
2019-09-17 10:17:30 -0400

    add thinning option

--- ggs2plink.sh
+++ ggs2plink.sh
@@ -3,6 +3,8 @@ set -e
 
 infile=$1
 outpref=$2
+thincount=$3
+seed=123
 
 echo 'producing .tped file...'
 cat $infile | tail -n +2 | cut -f2- | sed 's/0/A/g' | sed 's/1/C/g' > ${outpref}_tmp
@@ -14,6 +16,10 @@ cat $infile | head -n 1 | cut -f2- | sed 's/\//_/g' | sed 's/\t/\n/g' | awk 'NR 
 cat ${outpref}_tmp_iid | cut -d'_' -f1,2 > ${outpref}_tmp_fid
 paste ${outpref}_tmp_fid ${outpref}_tmp_iid | sed 's/$/ 0 0 1 1/' | head -n -1 > $outpref.tfam
 
-plink --tfile $outpref --make-bed --out $outpref
+if [ "$thincount" == 0 ]; then
+    plink --tfile $outpref --make-bed --out $outpref
+else
+    plink --seed $seed --tfile $outpref --thin-count $thincount --make-bed --out $outpref
+fi
 
 rm ${outpref}_tmp ${outpref}_tmp_leader 
