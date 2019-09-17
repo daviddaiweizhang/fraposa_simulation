a72f2145bbb08ba93bb8eca8142fd1e0b1defacc
David Zhang <zdavid0123@gmail.com>
2019-09-17 10:14:32 -0400

    update fraposa arg names

--- fraposa.sh
+++ fraposa.sh
@@ -7,4 +7,4 @@ method=$3
 out=$4
 
 fraposa="python $HOME/fraposa/fraposa_runner.py"
-$fraposa --method $method --dim_ref 2 --dim_stu 8 --dim_spikes 2 --out $out $ref $stu 
+$fraposa --method $method --dim_ref 2 --dim_stu 8 --dim_spikes 2 --out $out --stu_filepref $stu $ref 
