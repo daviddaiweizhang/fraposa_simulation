f8866568c8df517907382ae8e0950dcf6cc585b9
David Zhang <zdavid0123@gmail.com>
2019-09-17 10:58:57 -0400

    add smarter pref

--- pre_pca.sh
+++ pre_pca.sh
@@ -6,7 +6,11 @@ nstu=50
 for thincount in 0 50000 80000 90000; do
 for nref in `seq 250 125 750`; do
     let "n = $nref + $nstu"
-    pref=data/n${n}s${nstu}p${thincount}/a
+    if [ "$thincount" == 0 ]; then
+	    pref=data/n${n}s${nstu}/a
+    else
+	    pref=data/n${n}s${nstu}p${thincount}/a
+    fi
     log=${pref}_simu.log
     mkdir -p `dirname $log`
     bash simulate.sh $n $nstu $thincount $pref &> $log &
