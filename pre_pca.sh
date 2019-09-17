af25f3c1d9e07130342bf7bf412aec14f2bd9394
David Zhang <zdavid0123@gmail.com>
2019-09-17 10:24:10 -0400

    add thinning numbers

--- pre_pca.sh
+++ pre_pca.sh
@@ -3,10 +3,12 @@ set -e
 
 nstu=50
 
+for thincount in 0 50000 80000 90000; do
 for nref in `seq 250 125 750`; do
     let "n = $nref + $nstu"
-    pref=data/n${n}s${nstu}/a
+    pref=data/n${n}s${nstu}p${thincount}/a
     log=${pref}_simu.log
     mkdir -p `dirname $log`
-    bash simulate.sh $n $nstu $pref &> $log &
+    bash simulate.sh $n $nstu $thincount $pref &> $log &
+done
 done
