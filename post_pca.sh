263b62f46e336710abedaec64c80fa5fa989d924
David Zhang <zdavid0123@gmail.com>
2019-10-06 21:52:50 -0400

    fix minor bugs on rm

--- post_pca.sh
+++ post_pca.sh
@@ -9,7 +9,7 @@ for thincount in 0 10000 50000 80000; do
     else
         msd=data/msd_p${thincount}
     fi
-    rm -r $msd
+    rm -f $msd
     for nref in `seq 250 125 750`; do
         let "n = $nref + $nstu"
         if [ "$thincount" == 0 ]; then
