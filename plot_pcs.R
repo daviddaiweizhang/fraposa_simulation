aa0b1f67489308f7ca3687a6efda6b777b144254
David Zhang <zdavid0123@gmail.com>
2019-10-18 18:18:47 -0400

    add super-subplot title and legend

--- plot_pcs.R
+++ plot_pcs.R
@@ -59,7 +59,7 @@ msd.nullsd = round(sd(msd.nulldist), 3)
 
 msd.all = c(nrow(x.ref))
 png(paste0(inpref, ".png"), 2000, 2000)
-par(mfrow=c(2,2), cex=2)
+par(mfrow=c(2,2), cex=2, oma = c(4, 0, 4, 0))
 for(method in methods){
     x.stu = x.stu.all[[method]]
 
@@ -67,7 +67,8 @@ for(method in methods){
     c.stu = aggregate(x.stu[,c("PC1", "PC2")], by = list(x.stu$popu), FUN = mean)
     colnames(c.stu) = c("popu", "C1", "C2")
     stu.pch = 14
-    main = paste0(method, " with ref_size = ", nrow(x.ref), " (variation contributed from PC1-2: ", sum(pc.contrib), ")")
+
+    main = toupper(method)
     xlab = paste0("PC1 (contribution=", pc.contrib[1], ")")
     ylab = paste0("PC2 (contribution=", pc.contrib[2], ")")
     col.ref = as.integer(x.ref$popu) + 1
@@ -80,25 +81,28 @@ for(method in methods){
     x.stu$r = sqrt(rowSums((x.stu[,c("PC1", "PC2")] - x.stu[,c("C1", "C2")])^2))
     c.stu = merge(c.stu, aggregate(data.frame(r=x.stu$r), by=list(popu=x.stu$popu), FUN=function(x) quantile(x, radius)))
     lapply(1:nrow(c.stu), function(i) draw.circle(c.stu$C1[i], c.stu$C2[i], c.stu$r[i], border=1, lty=5, lwd=3))
-    if(method == "adp"){
-        des.ref = paste("Popu.", 1:4, "ref.")
-        legend("bottomright", legend=des.ref, pch=1, col=(1:4)+1)
-        des.stu = paste("Popu.", 1:4, "stu.")
-        legend("topright", legend=des.stu, pch=(1:4)+stu.pch, col=1)
-        legend("bottom", legend=paste(c("null MSD mean:", "null MSD sd:"), c(msd.nullmean, msd.nullsd)))
-    }
     msd = sqrt(mean(rowSums((c.stu[,c("C1", "C2")] - c.ref[,c("C1", "C2")])^2) / c.ref.scale))
     msd = round(msd, 3)
-    legend("bottomleft", legend=paste("MSD:", msd))
+    legend("bottomleft", legend=paste("sqrtMSD:", msd))
     msd.all = c(msd.all, msd)
 }
+supertitle = paste0(
+		    "PC 1&2 of simulated genotypes\n",
+		    "(reference size: ", nrow(x.ref),
+		    ", study size: ", nrow(x.stu),
+		    ", null sqrtMSD mean: ", msd.nullmean,
+		    ", null sqrtMSD sd: ", msd.nullsd, ")")
+mtext(supertitle, outer = TRUE, cex=3)
+des.ref = paste("Popu.", 1:4, "ref.")
+des.stu = paste("Popu.", 1:4, "stu.")
+legend(-110, -60, xpd="NA", legend=c(des.ref, des.stu), pch=c(rep(1,4), (1:4)+stu.pch), col=c((1:4)+1, rep(1,4)), ncol=8)
 dev.off()
 write(msd.all, paste0(inpref, "_msd"), ncolumns=length(msd.all), sep="\t")
 
 
 out.filename = paste0(inpref, "_scatter.png")
 print(out.filename)
-png(out.filename, 2000, 3000)
+png(out.filename, 2000, 6000)
 pairs = cbind(c(4,4,4,3,3,2), c(3,2,1,2,1,1))
 par(mfrow=c(nrow(pairs), num.pcs), cex=2)
 for(i in 1:nrow(pairs)){
