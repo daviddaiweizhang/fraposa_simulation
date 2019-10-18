8219f87c5b73cf2df7dae07ca4dc060280b350f7
David Zhang <zdavid0123@gmail.com>
2019-10-18 18:14:38 -0400

    adjust plotting

--- plot_trend.R
+++ plot_trend.R
@@ -4,7 +4,7 @@ msd.pref = ifelse(length(args) >= 1, args[1], "data/msd")
 x = read.table(msd.pref)
 methods = c("sp", "ap", "oadp", "adp")
 colnames(x) = c("nref", methods)
-ylim = c(0, 50)
+ylim = c(0.0, 0.4)
 
 lwd = 3
 cex = 2
@@ -14,7 +14,7 @@ for(i in 1:length(methods)){
     method = methods[i]
     if(i == 1){
         xlab = "Reference size"
-        ylab = "Squared root of MSD between centers"
+        ylab = "sqrtMSD"
         plot(x$nref, x[[method]], type="b", col=i, ylim=ylim, xlab=xlab, ylab=ylab, lwd=lwd, cex=cex)
     } else{
         points(x$nref, x[[method]], type="b", col=i, lwd=lwd, cex=cex)
