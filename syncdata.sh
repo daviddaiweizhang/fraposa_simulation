1d790d3ecc8c344e26d840138c62e81b21ce1c7e
David Zhang <zdavid0123@gmail.com>
2019-10-18 18:14:55 -0400

    make sync file pattern more flexible

--- syncdata.sh
+++ syncdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-rsync -am -e ssh --progress --include='*.png' --include='*/' --exclude='*' daiweiz@snowwhite.sph.umich.edu:~/fraposa_simulation/data/ data/
+pat=$1
+rsync -am -e ssh --progress --include="$pat" --include='*/' --exclude='*' daiweiz@snowwhite.sph.umich.edu:~/fraposa_simulation/data/ data/
