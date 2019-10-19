0dc28e250d18fee236061dc164ceb96a72a36525
David Daiwei Zhang <daiweiz@gl-login1.arc-ts.umich.edu>
2019-10-19 18:08:31 -0400

    detect whether geno and site files exist before running trace

--- trace.sh
+++ trace.sh
@@ -8,16 +8,20 @@ k=$4
 outpref=$5
 coordpref=$6
 
-vcf2geno=~/laser/vcf2geno/vcf2geno
-trace=~/laser/trace
+vcf2geno=vcf2geno
+trace=trace
 
 # convert plink --> vcf --> trace
-plink --bfile $refpref --keep-allele-order --recode vcf --out $refpref
-plink --bfile $stupref --keep-allele-order --recode vcf --out $stupref
-awk '{ print $1 "_" $2 "\t" $1 "\t" $2 }' $refpref.fam > ${outpref}_tmp 
-$vcf2geno --inVcf $refpref.vcf --out $refpref --updateID ${outpref}_tmp
-awk '{ print $1 "_" $2 "\t" $1 "\t" $2 }' $stupref.fam > ${outpref}_tmp 
-$vcf2geno --inVcf $stupref.vcf --out $stupref --updateID ${outpref}_tmp 
+for pref in $refpref $stupref; do
+    if [[ -f ${pref}.geno && -f ${pref}.site ]]; then
+        echo Using existing files: "${pref}.geno, ${pref}.site"
+    else
+        echo Files not found: "${pref}.geno, ${pref}.site"
+        plink --bfile $pref --keep-allele-order --recode vcf --out $pref
+        awk '{ print $1 "_" $2 "\t" $1 "\t" $2 }' $pref.fam > ${outpref}_tmp 
+        $vcf2geno --inVcf $pref.vcf --out $pref --updateID ${outpref}_tmp
+    fi
+done
 
 # convert ref pcs to coord file
 n_pcs=`head -n1 $coordpref.pcs | cut -f3- | wc -w`
