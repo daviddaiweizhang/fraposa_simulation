50fbfff8a5b742d64b805f6e0d4e78835b871068
David Zhang <contact@thedavidzhang.com>
2019-11-08 10:08:59 -0500

    retrieve all experiments from one log file. change to smarter grep

--- retrieve.sh
+++ retrieve.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 set -e
 
-n=$1
-root=data/runtimes
+root=$1
 cd $root
 
-cat `grep -l 'Study data: data/n'"$n"'s50/a_stu' *.log` > tmp
+rm -f tmp_out
+for n in `seq 300 125 800`; do
+    echo $n
+    for method in sp ap oadp; do
+        runtime=`grep -A 1 'data/n'"$n"'s50/tmp/''[0-9]\+.[0-9]\{9\}''/a_'"$method"'.pcs' *.log | tail -n1 | cut -d' ' -f3`
+        # runtime=$( cat tmp | grep -A 1 --no-group-separator a_"${method}".pcs | awk 'NR % 2 == 0' | cut -d' ' -f3 | Rscript -e 'x=scan("stdin", quiet=T); cat(mean(x), "\n")' )
+        echo $method $n $runtime >> tmp_out
+    done
 
-for method in sp ap oadp; do
-    runtime=$( cat tmp | grep -A 1 --no-group-separator a_"${method}".pcs | awk 'NR % 2 == 0' | cut -d' ' -f3 | Rscript -e 'x=scan("stdin", quiet=T); cat(mean(x), "\n")' )
-    echo $method $n $runtime
+    grep -h -A 100 -B 100 'data/n'"$n"'s50/tmp/''[0-9]\+.[0-9]\{9\}''/a_adp.ProPC.coord' *.log | grep "[0-9] [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\} 20[0-9]\{2\}" | grep -B 1 --no-group-separator 'Finished at: ' > tmp_timestamps
+    cat tmp_timestamps | awk 'NR % 2 == 1' > tmp_start
+    cat tmp_timestamps | awk 'NR % 2 == 0' | cut -d':' -f2- > tmp_end
+
+    for tmp_file in tmp_start tmp_end; do
+        rm -f ${tmp_file}_epoch
+        while read line; do
+            date --date="$line" +"%s" >> ${tmp_file}_epoch
+        done < $tmp_file
+    done
+
+    runtime_adp=$( paste tmp_start_epoch tmp_end_epoch | awk '{ print $2 - $1 }' | Rscript -e 'x=scan("stdin", quiet=T); cat(mean(x), "\n")' )
+    echo adp $n $runtime_adp >> tmp_out
 done
 
-
-cat tmp | grep 'Oct.*2019' | grep -B 1 --no-group-separator 'Finished at: ' > tmp_timestamps
-cat tmp_timestamps | awk 'NR % 2 == 1' > tmp_start
-cat tmp_timestamps | awk 'NR % 2 == 0' | cut -d':' -f2- > tmp_end
-
-for tmp_file in tmp_start tmp_end; do
-    rm -f ${tmp_file}_epoch
-    while read line; do
-        date --date="$line" +"%s" >> ${tmp_file}_epoch
-    done < $tmp_file
-done
-
-runtime_adp=$( paste tmp_start_epoch tmp_end_epoch | awk '{ print $2 - $1 }' | Rscript -e 'x=scan("stdin", quiet=T); cat(mean(x), "\n")' )
-echo adp $n $runtime_adp
-
+cut -d' ' -f3 tmp_out | Rscript -e 'x=scan("stdin"); x=matrix(x, 5, 4, byrow=T); x=round(x,2); colnames(x)=c("sp", "ap", "oadp", "adp"); rownames(x)=seq(300, 800, 125); write.table(x, "runtimes.tab", quote=F)'
+cat runtimes.tab
 rm tmp tmp_*
